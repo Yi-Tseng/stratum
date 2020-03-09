b27ad889c7d154467b93899cf8a44c656b53b057
Yi Tseng <yi@opennetworking.org>
2020-03-09 13:11:17 -0700

    convert coverage to lcov

---
+++ .circleci/convert_covs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SO_FILES=$(find bazel-bin/stratum/ -name *.so -type f)
+
+touch _coverage.lcov
+
+for f in $SO_FILES
+do
+  llvm-cov export --format lcov --instr-profile _coverage.dat $f >> _coverage.lcov
+done
